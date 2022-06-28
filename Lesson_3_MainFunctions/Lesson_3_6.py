5fc59c395c4953f093ea59fa202cc0042c6c5fb8
bpiasetski <xkorvin@gmail.com>
2022-06-29 00:08:18 +0300

    Add action_request

--- Lesson_3_MainFunctions/Lesson_3_6.py
+++ Lesson_3_MainFunctions/Lesson_3_6.py
@@ -1,1 +1,11 @@
-action_request = input('Enter command "read" or ""copy"')
+while True:
+    action_request = input('Enter command "read" or ""copy":')
+    if action_request == "read":
+        file_path = input("Write the path to the file whose contents you want to view:")
+        handler = open(file_path, 'r')
+        print(handler.read())
+        handler.close()
+    elif action_request == "copy":
+        file_path = input("Write the path to the file you want to copy:")
+    else:
+        print("Unknown command!")
